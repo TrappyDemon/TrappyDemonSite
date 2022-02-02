23fc0607653ec400f2af1fea1ae097bae8777b6d
TrappyDemon <98855886+TrappyDemon@users.noreply.github.com>
2022-02-02 01:15:05 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -14,9 +14,13 @@ searchBtn.addEventListener("click" , () =>{
     sidebar.classList.remove("close");
 })
 
+modeSwitch.addEventListener("click" , () =>{
     body.classList.toggle("dark");
     
-  
+    if(body.classList.contains("dark")){
+        modeText.innerText = "Light mode";
+    }else{
+        modeText.innerText = "Dark mode";
         
     }
 });
